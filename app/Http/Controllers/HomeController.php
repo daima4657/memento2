116db0255ed3a18cff27e293e8a74f72949d4c8b
daima7613 <freeasabird4657@gmail.com>
2020-09-27 23:02:52 +0900

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\models\Book;
+use App\models\Book;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
